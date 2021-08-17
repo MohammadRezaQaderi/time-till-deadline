53a2407b1a18c6809ab0d15fca2a20dd833acf36
MohammadRezaQaderi <Mrq112775@outlook.com>
2021-08-17 16:28:18 +0430

    The use openpyxl to some basic operations

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-message = "This is a simple message"  # string
+import openpyxl as xl
+
+"""message = "This is a simple message"  # string
 day = 9  # integer
 price = 19.99  # float
 valid = True  # or can be False Boolean
 list_of_months = ['January', 'February', 'March', 'April']  # list
 set_of_day = {10, 15, 35, 90}  # set
-days_and_units = {'days': 20, "unit": "hours"}  # dictionary
+days_and_units = {'days': 20, "unit": "hours"}  # dictionary"""
+
+inv_file = xl.load_workbook("inventory.xlsx")
+product_list = inv_file["Sheet1"]
+product_per_supplier = {}
+total_value_per_supplier = {}
+
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+
+    # product per supplier
+    if supplier_name in product_per_supplier:
+        product_per_supplier[supplier_name] = product_per_supplier.get(supplier_name) + 1
+    else:
+        product_per_supplier[supplier_name] = 1
+
+    # calculate total value per supplier
+    if supplier_name in total_value_per_supplier:
+        current_value = total_value_per_supplier.get(supplier_name)
+        total_value_per_supplier[supplier_name] = current_value + inventory * price
+    else:
+        total_value_per_supplier[supplier_name] = inventory * price
+
+print(product_per_supplier)
+print(total_value_per_supplier)
