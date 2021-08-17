0d224e0d97184712c4add3794856ba636fc43f85
MohammadRezaQaderi <Mrq112775@outlook.com>
2021-08-17 11:17:31 +0430

    The countdown to Goal with python use datetime

--- time-till-deadline.py
+++ time-till-deadline.py
@@ -1,7 +1,17 @@
 import datetime
 
 date_time = input("enter your goal with a deadline separated by colon\n")
+input_list = date_time.split(":")
 
+goal = input_list[0]
+deadline = input_list[1]
+
+date_deadline = datetime.datetime.strptime(deadline, "%d.%m.%Y")
+today = datetime.datetime.today()
+
+time_till = date_deadline - today
+
+print(f"Dear user! Time remaining to your goal : {goal} is {time_till.days} days")
 
 
 
